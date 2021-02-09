a11d31590b475ff1f24502845baeaf9d3bd50457
Refticx <refticx@gmail.com>
2021-02-09 11:51:57 +0900

    【UI追加】デバッグ専用UI追加

---
+++ trackID3TagSwitcher/trackID3TagSwitcher/DebugText.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace trackID3TagSwitcher
+{
+    partial class DebugText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent( )
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(237, 447);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(336, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(237, 447);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(255, 41);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 19);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Minimum = -100;
+            this.numericUpDown1.Maximum = 100;
+            // 
+            // DebugText
+            // 
+            this.ClientSize = new System.Drawing.Size(626, 471);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "DebugText";
+            this.Load += new System.EventHandler(this.AccountForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout( );
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
---
+++ trackID3TagSwitcher/trackID3TagSwitcher/DebugText.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace trackID3TagSwitcher
+{
+    public partial class DebugText : Form
+    {
+        public DebugText( )
+        {
+            InitializeComponent( );
+        }
+        private void AccountForm_Load( object sender , EventArgs e )
+        {
+
+        }
+
+        private void button1_Click( object sender , EventArgs e )
+        {
+            string txt1 = this.richTextBox1.Text;
+            string txt2 = String.Empty;
+            for ( int i = 0 ; i < txt1.Length ; i++ )
+            {
+                txt2 += (char)( (int)txt1[i] + this.numericUpDown1.Value );
+            }
+            this.richTextBox2.Text = txt2;
+        }
+    }
+}
